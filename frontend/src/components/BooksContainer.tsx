390357603fe5e83dc5ca3118588678ef3b780953
main <claudia.raphael73@gmail.com>
2025-10-26 18:30:22 -0300

    create BooksContainer component

---
+++ frontend/src/components/BooksContainer.tsx
@@ -0,0 +1,25 @@
+import { Book } from '../types/book';
+import BookCard from './BookCard';
+import './BooksContainer.css';
+
+interface BooksContainerProps {
+    books: Book[];
+}
+
+function BooksContainer({ books }: BooksContainerProps) {
+    return (
+        <div className="books-container" id="books">
+            {books.length === 0 ? (
+                <div className="no-books">
+                    <p>Nenhum livro encontrado na biblioteca</p>
+                </div>
+            ) : (
+                books.map((book) => (
+                    <BookCard key={book.id} book={book} />    
+                ))
+            )}
+        </div>
+    );
+}
+
+export default BooksContainer;
